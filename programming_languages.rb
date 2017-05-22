01e8fe302c1007c9cb0a015ea5e19074c1941ba4
Joe Teichman <kaalm1@gmail.com>
2017-05-22 17:31:23 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  newHash = {}
+  languages.each do |x,y|
+    y.each do |main,data|
+      if newHash[main].nil?
+        newHash[main] = {}
+        newHash[main][:style] = []
+      end
+      newHash[main][:type] = data.values.join
+      newHash[main][:style] << x
+    end
+  end
+  newHash
 end
